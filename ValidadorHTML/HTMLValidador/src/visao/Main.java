2db3488a8befce7939bdee320915959e334a9dd6
Yan Carvalho Medeiros <devyancarvalho15@gmail.com>
2026-06-23 21:04:00 -0300

    00

--- ValidadorHTML/HTMLValidador/src/visao/Main.java
+++ ValidadorHTML/HTMLValidador/src/visao/Main.java
@@ -1,5 +1,12 @@
 package visao;
 
-public class Main {
+import javax.swing.SwingUtilities;
 
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            TelaPrincipal tela = new TelaPrincipal();
+            tela.setVisible(true);
+        });
+    }
 }
